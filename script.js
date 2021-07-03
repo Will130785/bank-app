e9f411f797c69ca00e0e9e36afc5727412a3c65c
Will <will_constable@msn.com>
2021-07-03 13:15:57 +0100

    Add balance and summary calculations

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const user = document.querySelector('.login__input--user')
 const pin = document.querySelector('.login__input--pin')
 const login = document.querySelector('.login__btn')
 const mainApp = document.querySelector('.app')
+const balance = document.querySelector('.balance__value')
+const amtSpent = document.querySelector('.summary__value--out')
+const amtEarnt = document.querySelector('.summary__value--in')
+const intRate = document.querySelector('.summary__value--interest')
 
 // Data
 const accountData = [
@@ -37,54 +41,30 @@ const accountData = [
   }
 
 ]
-// const account1 = {
-//   owner: 'Jonas Schmedtmann',
-//   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-//   interestRate: 1.2, // %
-//   pin: 1111
-// }
 
-// const account2 = {
-//   owner: 'Jessica Davis',
-//   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-//   interestRate: 1.5,
-//   pin: 2222
-// }
-
-// const account3 = {
-//   owner: 'Steven Thomas Williams',
-//   movements: [200, -200, 340, -300, -20, 50, 400, -460],
-//   interestRate: 0.7,
-//   pin: 3333
-// }
-
-// const account4 = {
-//   owner: 'Sarah Smith',
-//   movements: [430, 1000, 700, 50, 90],
-//   interestRate: 1,
-//   pin: 4444
-// }
+// Set empty logged in User object
+const loggedInUser = {}
 
 // Handle login function
 const handleLogin = (e) => {
   e.preventDefault()
-  // if (user.value === 'Will' && Number(pin.value) === 6302) {
-  //   console.log('Correct Login')
-  //   // If credentials are correct display app screen
-  //   mainApp.style.opacity = 1
-  // } else {
-  //   console.log('Incorrect credentials')
-  //   // If incorrect make sure app screen is not displayed
-  //   mainApp.style.opacity = 0
-  // }
   accountData.forEach(item => {
-    console.log(item.pin)
-    console.log(Number(pin.value))
     if (item.user === user.value && item.pin === Number(pin.value)) {
       console.log('Correct')
       // If credentials are correct display app screen
       mainApp.style.opacity = 1
-      return item
+      console.log(item)
+      // Set logged in user details
+      loggedInUser.name = item.owner
+      loggedInUser.movements = item.movements
+      loggedInUser.interestRate = item.interestRate
+      loggedInUser.user = item.user
+      loggedInUser.pin = item.pin
+      // Get balance
+      getAccountBalance(loggedInUser)
+      // Get summary
+      getAccountSummary(loggedInUser)
+      return loggedInUser
     } else {
       // If incorrect make sure app screen is not displayed
       // mainApp.style.opacity = 0
@@ -92,5 +72,36 @@ const handleLogin = (e) => {
   })
 }
 
+// Function to get and display balance
+const getAccountBalance = (data) => {
+  // Get balance
+  const userBalance = data.movements.reduce((acc, curr) => {
+    return acc + curr
+  })
+  balance.innerHTML = userBalance
+  return userBalance
+}
+
+// Function to get and display account summary
+const getAccountSummary = (data) => {
+  console.log('Fetching account data')
+  console.log('*********************')
+  console.log(data)
+  // Calculate amount spent and outout to DOM
+  const spent = data.movements.map(item => item < 0 ? item : 0)
+    .reduce((acc, curr) => acc + curr)
+  amtSpent.innerHTML = spent
+  console.log(spent)
+  // Calculate amount earnt and output to DOM
+  const earnt = data.movements.map(item => item > 0 ? item : 0)
+    .reduce((acc, curr) => acc + curr )
+  amtEarnt.innerHTML = earnt
+  console.log(earnt)
+  // Calculate interest earnt
+  const interest = data.movements.reduce((acc, curr) => (acc + curr) * data.interestRate * 1)
+  intRate.innerHTML = interest
+  console.log(interest)
+}
+
 // Event listeners
-const activeUser = login.addEventListener('click', handleLogin)
+login.addEventListener('click', handleLogin)
